6933de85899ddf3d6497a32a228d1432cb70cf32
xStarRier <61316530+xStarRier@users.noreply.github.com>
2024-08-23 15:59:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function changeBC() { 
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+
+  document.body.style.background ="rgb(" + red + ", " + green + ", " + blue + ")";
+
+
+}
+
+
+
